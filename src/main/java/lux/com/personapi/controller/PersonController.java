f714c71d201e3da9dce42cbbdced5fe0c231b750
lfaraujo01 <lucas_araujo01@hotmail.com>
2021-08-09 21:46:37 -0300

    correction at properties

--- src/main/java/lux/com/personapi/controller/PersonController.java
+++ src/main/java/lux/com/personapi/controller/PersonController.java
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/people")
+@RequestMapping("/api/v1")
 public class PersonController {
 
     @GetMapping
     public String getbook(){
-        return "Apliação no Heroku";
+        return "to online agora segura o pogramador";
     }
 }
