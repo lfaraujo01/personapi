78fd286d8f5543070304276d097968f07f0512e8
lfaraujo01 <lucas_araujo01@hotmail.com>
2021-08-14 08:25:16 -0300

    ready to create first person

--- src/main/java/lux/com/personapi/controller/PersonController.java
+++ src/main/java/lux/com/personapi/controller/PersonController.java
@@ -1,18 +1,31 @@
 package lux.com.personapi.controller;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lux.com.personapi.entity.Person;
+import lux.com.personapi.repository.PersonRepository;
+import lux.com.personapi.response.MessageResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
 
-    @GetMapping
-    public String getbook(){
+    private PersonRepository personRepository;
 
 
-        return "to online agora segura o pogramador";
+    @Autowired
+    public PersonController (PersonRepository personRepository){
+        this.personRepository = personRepository;
+
+    }
+
+    @PostMapping
+    public MessageResponseDTO createPerson(@RequestBody Person person){
+        Person savedPerson=personRepository.save(person);
+        return MessageResponseDTO
+                .builder()
+                .message("Create person with ID " + savedPerson.getId())
+                .build();
     }
 }
